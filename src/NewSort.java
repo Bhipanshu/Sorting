97ebc7ede0e01d128a08778b4ec33eacda92f61e
e21cseu0250 <e21cseu0250@SCSET206BLA03.bennett.edu.in>
2022-09-15 14:50:57 +0530

    new

--- src/NewSort.java
+++ src/NewSort.java
@@ -12,7 +12,7 @@ public class NewSort {
 		        //Displaying elements of original array    
 		        System.out.println("Elements of original array: ");    
 		        for (int i = 0; i < arr.length; i++) {     
-		            System.out.print(arr[i] + " ");    
+		            System.out.print(arr[i] + "   ");    
 		        }    
 		            
 		        //Sort the array in ascending order    
